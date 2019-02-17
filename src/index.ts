f21a236b6849dc2aa19ff94b8a15e3e0c670055e
xg4 <xingor4@gmail.com>
2019-02-17 16:37:43 +0800

    chore: add tslint

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ interface IOptions {
   type?: string
   quality?: number
   text?: string
-  gradient?: [number, string][]
+  gradient?: Array<[number, string]>
 }
 
 interface IDefaultOptions {
@@ -19,65 +19,13 @@ interface IDefaultOptions {
   type: string
   quality: number
   text: string
-  gradient?: [number, string][]
+  gradient?: Array<[number, string]>
 }
 
 /**
  * @description convert text to image by canvas
  */
 export default class TextImage {
-  private static instance: TextImage
-
-  static create(...args: any[]) {
-    if (!this.instance) {
-      this.instance = new this(...args)
-    }
-    return this.instance
-  }
-
-  defaultOptions = {
-    fontSize: 30,
-    color: '#000000',
-    fontFamily: 'Arial',
-    fontWeight: 'normal',
-    type: 'image/png',
-    quality: 0.92,
-    text: ''
-  }
-
-  currentOptions = {
-    ...this.defaultOptions
-  }
-
-  options: IDefaultOptions = {
-    ...this.defaultOptions
-  }
-
-  image?: HTMLImageElement | null
-
-  c: HTMLCanvasElement
-
-  ctx: CanvasRenderingContext2D
-
-  constructor(options: IOptions = {}) {
-    this.c = document.createElement('canvas')
-    this.ctx = this.c.getContext('2d') as CanvasRenderingContext2D
-
-    this.setDefaultOptions(options)
-  }
-
-  setDefaultOptions(options: IOptions = {}) {
-    Object.assign(this.currentOptions, options)
-  }
-
-  resetDefaultOptions() {
-    this.currentOptions = { ...this.defaultOptions }
-  }
-
-  private parseOptions(text: string | IOptions): IOptions {
-    return isObj(text) ? (text as IOptions) : ({ text } as IOptions)
-  }
-
   get width() {
     return this.c.width
   }
@@ -98,8 +46,126 @@ export default class TextImage {
     }`
   }
 
+  public static create(...args: any[]) {
+    if (!this.instance) {
+      this.instance = new this(...args)
+    }
+    return this.instance
+  }
+  private static instance: TextImage
+
+  public defaultOptions = {
+    fontSize: 30,
+    color: '#000000',
+    fontFamily: 'Arial',
+    fontWeight: 'normal',
+    type: 'image/png',
+    quality: 0.92,
+    text: '',
+  }
+
+  public currentOptions = {
+    ...this.defaultOptions,
+  }
+
+  public options: IDefaultOptions = {
+    ...this.defaultOptions,
+  }
+
+  public image?: HTMLImageElement | null
+
+  public c: HTMLCanvasElement
+
+  public ctx: CanvasRenderingContext2D
+
+  constructor(options: IOptions = {}) {
+    this.c = document.createElement('canvas')
+    this.ctx = this.c.getContext('2d') as CanvasRenderingContext2D
+
+    this.setDefaultOptions(options)
+  }
+
+  public setDefaultOptions(options: IOptions = {}) {
+    Object.assign(this.currentOptions, options)
+  }
+
+  public resetDefaultOptions() {
+    this.currentOptions = { ...this.defaultOptions }
+  }
+
+  public setImage(imgUrl?: string): Promise<HTMLImageElement> {
+    return new Promise((resolve, reject) => {
+      if (!imgUrl || !isSrc(imgUrl)) {
+        this.image = null
+        resolve()
+        return
+      }
+
+      if (this.image && this.image.src === imgUrl) {
+        resolve()
+        return
+      }
+
+      this.image = null
+
+      const img = new Image()
+      img.onload = () => {
+        this.image = img
+        resolve(img)
+      }
+      img.onerror = reject
+      img.src = imgUrl
+    })
+  }
+
+  public toDataURL(text: string | IOptions) {
+    this.options = {
+      ...this.currentOptions,
+      ...this.parseOptions(text),
+    }
+
+    this.draw()
+
+    if (!(this.width && this.height)) {
+      return
+    }
+    return this.c.toDataURL(this.options.type, this.options.quality)
+  }
+
+  public createURL(text: string | IOptions): Promise<string> {
+    return new Promise((resolve) => {
+      this.options = {
+        ...this.currentOptions,
+        ...this.parseOptions(text),
+      }
+
+      this.draw()
+
+      if (!(this.width && this.height)) {
+        return
+      }
+      this.c.toBlob(
+        (blob) => {
+          resolve(URL.createObjectURL(blob))
+        },
+        this.options.type,
+        this.options.quality,
+      )
+    })
+  }
+
+  public destroyURL(url: string) {
+    return URL.revokeObjectURL(url)
+  }
+
+  private parseOptions(text: string | IOptions): IOptions {
+    return isObj(text) ? (text as IOptions) : ({ text } as IOptions)
+  }
+
   private drawImage() {
-    if (!this.image) return
+    if (!this.image) {
+      return
+    }
 
     this.ctx.save()
     const width = this.image.width
@@ -109,7 +175,7 @@ export default class TextImage {
       this.width / 2 - width / 2,
       this.height / 2 - height / 2,
       width,
-      height
+      height,
     )
     this.ctx.restore()
   }
@@ -119,7 +185,7 @@ export default class TextImage {
 
     if (this.options.gradient) {
       const gradient = this.ctx.createLinearGradient(0, 0, this.width, 0)
-      this.options.gradient.forEach(v => {
+      this.options.gradient.forEach((v) => {
         gradient.addColorStop(...v)
       })
       this.ctx.fillStyle = gradient
@@ -153,66 +219,5 @@ export default class TextImage {
     // draw
     this.drawImage()
     this.drawText()
-  }
-
-  setImage(imgUrl?: string): Promise<HTMLImageElement> {
-    return new Promise((resolve, reject) => {
-      if (!imgUrl || !isSrc(imgUrl)) {
-        this.image = null
-        resolve()
-        return
-      }
-
-      if (this.image && this.image.src === imgUrl) {
-        resolve()
-        return
-      }
-
-      this.image = null
-
-      const img = new Image()
-      img.onload = () => {
-        this.image = img
-        resolve(img)
-      }
-      img.onerror = reject
-      img.src = imgUrl
-    })
-  }
-
-  toDataURL(text: string | IOptions) {
-    this.options = {
-      ...this.currentOptions,
-      ...this.parseOptions(text)
-    }
-
-    this.draw()
-
-    if (!(this.width && this.height)) return
-    return this.c.toDataURL(this.options.type, this.options.quality)
-  }
-
-  createURL(text: string | IOptions): Promise<string> {
-    return new Promise(resolve => {
-      this.options = {
-        ...this.currentOptions,
-        ...this.parseOptions(text)
-      }
-
-      this.draw()
-
-      if (!(this.width && this.height)) return
-      this.c.toBlob(
-        blob => {
-          resolve(URL.createObjectURL(blob))
-        },
-        this.options.type,
-        this.options.quality
-      )
-    })
-  }
-
-  destroyURL(url: string) {
-    return URL.revokeObjectURL(url)
   }
 }
